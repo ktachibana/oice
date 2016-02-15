789e98d581192fa15b98fb8bdaa47551e67c32e4
Kenichi Tachibana <deadzone@be.to>
2016-02-15 23:42:11 +0900

    簡素に

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ post '/' do
   command = "cat #{file.path}|sox -q -V1 - -b 16 -r 16k -c 1 -t .wav - | julius -C my.jconf"
   str = `#{command}`
 
-  sentence = str[PATTERN, 1] || fail('no sentence:')
+  sentence = str[PATTERN, 1] || (break 204)
   words = sentence.split(/\s+/)
 
   results = []
