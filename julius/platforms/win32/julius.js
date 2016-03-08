4db72b2e590baa8126a97deee32947a10c91790d
Kenichi Tachibana <deadzone@be.to>
2016-03-09 00:37:39 +0900

    winで一時ファイルを利用

--- julius/platforms/win32/julius.js
+++ julius/platforms/win32/julius.js
@@ -1,17 +1,23 @@
 const child = require('child_process');
 const path = require('path');
+const tmp = require('tmp');
+tmp.setGracefulCleanup();
 
 module.exports = function(audioFilePath) {
   // stdin経由だとうまく行かなかった(win版特有の症状)ためファイルを経由する
+  const tmpFile = tmp.fileSync({ prefix: 'charm-', postfix: '.wav' });
+
   child.spawnSync(
     path.resolve(__dirname, 'sox-14.4.2/sox.exe'),
-    [audioFilePath, '-t', '.wav', '-r', '16k', '-b', '16', '-c', '1', 'out.wav'] // TODO: tempfile
+    [audioFilePath, '-t', '.wav', '-r', '16k', '-b', '16', '-c', '1', tmpFile.name]
   );
 
   const juliusResult = child.spawnSync(
     path.resolve(__dirname, 'julius-4.3.1-win32bin/bin/julius-4.3.1.exe'),
     ['-C', path.resolve(__dirname, '../../charm.jconf'), '-input', 'file'],
-    { input: 'out.wav' }
+    { input: tmpFile.name }
   );
+
+  tmpFile.removeCallback();
   return juliusResult.stdout.toString();
 };
